f7268fbb2eea2e6e3df72aa3b35a6f894225958f
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-01-27 14:50:29 +0700

    Create Miningsc.sh

---
+++ Miningsc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	./xmrig -a randomx --url randomx.rplant.xyz:17080 --tls --user 48FGBZqmhyvQPWkDkVFXzqXEgxEQdV6rX1M4K6rW86g4PbQ9bBwj9vKFhfhPW4D78bh3HRYKhWC2KjQDH5535htDAcxe9m5
+	sleep 5
+done
