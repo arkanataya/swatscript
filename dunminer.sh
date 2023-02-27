4e67f59933d64400dc243c9087c3437283a0d4aa
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-02-27 16:34:31 +0700

    Create dunminer.sh

---
+++ dunminer.sh
@@ -0,0 +1,6 @@
+# Corp
+sudo apt install python3 python3-dev python3-pip -y
+wget https://github.com/revoxhere/duino-coin/archive/refs/tags/3.4.zip
+unzip 3.4.zip
+cd duino-coin-3.4
+python3 PC_Miner.py
