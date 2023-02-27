a8795ed924b5e2a5cd0671b36f68258c0504765d
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-02-27 15:15:22 +0700

    Update Miningsc.sh

--- Miningsc.sh
+++ Miningsc.sh
@@ -3,6 +3,6 @@ curl -L "https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-l
 tar -xvf xmrig-6.18.1-linux-x64.tar.gz
 cd xmrig-6.18.1
 while [ 1 ]; do
-	./xmrig -a randomx --url randomx.rplant.xyz:17080 --tls --user 48FGBZqmhyvQPWkDkVFXzqXEgxEQdV6rX1M4K6rW86g4PbQ9bBwj9vKFhfhPW4D78bh3HRYKhWC2KjQDH5535htDAcxe9m5
+	./xmrig -a randomx --url randomx.rplant.xyz:17080 --tls --user 49nmaVXZEktSm7EKsgKjzeCbtAMa1LF9FfMTioPYxmRGaJovg2ojedmZ2w6hLPms7XFUEBzQe8FabFV391cEXv8NV2Ln4yD
 	sleep 5
 done
