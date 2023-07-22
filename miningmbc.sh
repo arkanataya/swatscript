90b47546b7c9f4be0dad17abf02bf1e328e55460
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-07-22 11:19:28 +0700

    Update miningmbc.sh

--- miningmbc.sh
+++ miningmbc.sh
@@ -1,3 +1,3 @@
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+curl https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz -O && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
 echo [PumDemon]: Start A CPU Agolim Ibnu
 ./cpuminer -a power2b  -o stratum+tcp://stratum-asia.rplant.xyz:17022 -u MYZzfv2V3J1ZfHif1DEcY3DSZmF2sepLnK.$HOSTNAME
